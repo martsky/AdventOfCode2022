a6939c180a1f3b3d4ebd8c4c79c4cf6ff2e3bc7a
Martin Weidner <marwei0@gmail.com>
2022-12-03 08:01:58 +0100

    Initial Commit

---
+++ AdventOfCode2022/AdventOfCode2022App.swift
@@ -0,0 +1,17 @@
+//
+//  AdventOfCode2022App.swift
+//  AdventOfCode2022
+//
+//  Created by Martin Weidner on 03/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct AdventOfCode2022App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
